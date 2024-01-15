b625ccff38f48383ce5d83b775d22f1129bb2987
DeimisM <deimis.mardosas@outlook.com>
2024-01-15 16:15:27 +0200

    financial math text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,30 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public FollowPath player;
 
-    // Example dice roll function
-    public void RollDice()
-    {
-        int diceResult = Random.Range(1, 7);
+    public TMP_Text salaryText;
+    public TMP_Text jobTitleText;
+    string job = "Police Officer Salary:";
+    int salary = 2500;
 
-        // Set the current waypoint based on the dice result, with looping behavior
-        player.currentWaypoint = (player.currentWaypoint + diceResult) % player.waypoints.Length;
+    public TMP_Text taxesText;
+    int taxes;
 
-        // Allow movement
-        player.moveAllowed = true;
-    }
+    int totalIncome;
+    int totalExpenses;
+    int payday;
+    public TMP_Text totalIncomeText;
+    public TMP_Text totalExpensesText;
+    public TMP_Text paydayText;
 
     private void Start()
     {
+        jobTitleText.text = job;
+        salaryText.text = "$" + salary.ToString();
 
+        taxes = salary / 100 * 35;
+        taxesText.text = "$" + taxes.ToString();
     }
 
     private void Update()
     {
+        totalExpenses = taxes;
+        totalIncome = salary;
+        payday = totalIncome - totalExpenses;
 
+        totalIncomeText.text = "$" + totalIncome.ToString();
+        totalExpensesText.text = "$" + totalExpenses.ToString();
+        paydayText.text = "$" + payday.ToString();
     }
+
+    public void RollDice()
+    {
+        int diceResult = Random.Range(1, 7);
+
+        // Set the current waypoint based on the dice result with looping behavior
+        player.currentWaypoint = (player.currentWaypoint + diceResult) % player.waypoints.Length;
+
+        player.moveAllowed = true;
+    }
+
+
 }
