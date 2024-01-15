3995a630f23ea23f1abc2c43911baf9e2f87061f
DeimisM <deimis.mardosas@outlook.com>
2024-01-15 16:52:23 +0200

    expenses text and math

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,34 +12,69 @@ public class GameManager : MonoBehaviour
     string job = "Police Officer Salary:";
     int salary = 2500;
 
-    public TMP_Text taxesText;
     int taxes;
+    public TMP_Text taxesText;
 
     int totalIncome;
     int totalExpenses;
     int payday;
     public TMP_Text totalIncomeText;
     public TMP_Text totalExpensesText;
+    public TMP_Text totalExpensesText1;
     public TMP_Text paydayText;
+
+    int cash;
+    public TMP_Text cashText;
+
+    int mortgage;
+    int mortgagePayment;
+    public TMP_Text mortgageText;
+    public TMP_Text mortgagePaymentText;
+
+    int otherExpenses;
+    public TMP_Text otherExpensesText;
+
+    int loan;
+    int loanPayment;
+    public TMP_Text loanText;
+    public TMP_Text loanPaymentText;
 
     private void Start()
     {
         jobTitleText.text = job;
         salaryText.text = "$" + salary.ToString();
 
-        taxes = salary / 100 * 35;
+        taxes = salary / 100 * 30;
         taxesText.text = "$" + taxes.ToString();
+
+        cash = 520;
+        loan = 1000;
+
+        mortgage = 40000;
+        mortgagePayment = mortgage / 100;
+        mortgageText.text = "$" + mortgage.ToString();
+        mortgagePaymentText.text = "$" + mortgagePayment.ToString();
+
+        otherExpenses = salary / 100 * 17;
+        otherExpensesText.text = otherExpenses.ToString();
     }
 
     private void Update()
     {
-        totalExpenses = taxes;
+        totalExpenses = taxes + mortgagePayment + otherExpenses + loanPayment;
         totalIncome = salary;
         payday = totalIncome - totalExpenses;
 
         totalIncomeText.text = "$" + totalIncome.ToString();
         totalExpensesText.text = "$" + totalExpenses.ToString();
+        totalExpensesText1.text = "$" + totalExpenses.ToString();
         paydayText.text = "$" + payday.ToString();
+
+        cashText.text = "$" + cash.ToString();
+
+        loanPayment = loan / 100 * 10;
+        loanText.text = "$"+ loan.ToString();
+        loanPaymentText.text = "$" + loanPayment.ToString();
     }
 
     public void RollDice()
